783344a3f8922c499926c0bd9e608bf2f479d436
IvanT <ivantenopalaperez@gmail.com>
2022-09-15 15:58:39 -0500

    Desarrollo de funcion de encriptacion

--- encriptador.js
+++ encriptador.js
@@ -1,33 +1,66 @@
 function encriptar() {
-    var rectangulo = document.getElementById("rectangulo");
+    
     var imagen = document.getElementById("mensaje-encriptar");
     var leyenda = document.getElementById("leyenda");
     var mensaje = document.getElementById("mensaje").value;
-    var textarea = document.createElement("textarea");
+    var textarea = document.getElementById("enc-des");
 
     if (mensaje != "") {
+
         imagen.style.display = "none";
         leyenda.style.display = "none";
+        textarea.style.visibility = "visible";
 
-        textarea.disabled = "disabled";
         textarea.value = encriptarMensaje(mensaje);
-        textarea.className = "mensaje-respuesta";
-        rectangulo.appendChild(textarea);
+
     }
 }
 
 function encriptarMensaje(texto) {
-    var a = "ai";
-    var e = "enter";
-    var i = "imes";
-    var o = "ober";
-    var u = "ufat";
 
-    for (let index = 0; index < texto.length; index++) {
-        if (texto.charAt(index) == 'a') {
-            texto.charAt(index) = 'ai';
-        }
+    var a = 'ai';
+    var e = 'enter';
+    var i = 'imes';
+    var o = 'ober';
+    var u = 'ufat';
+    var tem = [];
+
+    tem.length = texto.length;
+
+    for (let index = 0; index < tem.length; index++) {
+        tem[index] = texto.charAt(index);
     }
+
+    for (let l = 0; l < tem.length; l++) {
+
+        if (tem[l] == 'a') {
+
+            tem[l] = a;
+
+        }else if (tem[l] == 'e') {
+
+            tem[l] = e;
+
+        }else if(tem[l] == 'i'){
+
+            tem[l] = i;
+
+        }else if(tem[l] == 'o'){
+
+            tem[l] = o;
+
+        }else if(tem[l] == 'u'){
+            
+            tem[l] = u;
+
+        }
+        
+    }
+    
+    texto = tem.toString().replace(/,/g,"");
+    console.log(texto);
+    /*texto = texto.replaceAll('a',a);*/
+
     return texto;
 }
 
